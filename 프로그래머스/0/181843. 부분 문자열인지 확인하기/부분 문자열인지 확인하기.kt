33b92474577986ca7eb0628afc424f7f85c86b11
yg-cho <cyg4484@gmail.com>
2024-10-26 20:36:05 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 10.35 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, target: String):Int {
+        return if(my_string.contains(target)){
+         1
+        } else {
+         0
+        }
+    }
+}
